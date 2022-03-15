5c3607680a8bd2609b1dadb1cde85eab4b0b51a2
aamp19 <51056791+aamp19@users.noreply.github.com>
2022-03-15 14:39:19 -0400

    Create LandingPage.jsx

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from '../parts/Heading'
+import Register from '../parts/Register'
+const LandingPage = () => {
+    return (
+        <div>
+            <Heading />
+            <Register />
+        </div>
+    )
+}
+
+export default LandingPage
