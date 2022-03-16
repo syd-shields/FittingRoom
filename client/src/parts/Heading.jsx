ff46a3658620c07d5d8b6f38e2f2027b30dde3e6
Syd Shields <sshields750@gmail.com>
2022-03-16 13:47:50 -0400

    empty commit

---
+++ client/src/parts/Heading.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Heading = () => {
+    return (
+        <div>
+
+            <div class="header">
+                <h1>Outfit fitter</h1>
+                <p>Virtually wear outfits with the click of a button</p>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Heading
