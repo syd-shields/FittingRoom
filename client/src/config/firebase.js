3a01c24218af109a509cb0f12cbd9ea845bf8412
Syd Shields <sshields750@gmail.com>
2022-03-15 20:37:44 -0400

    described client server architecture

---
+++ client/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_AqwPpLAN4Hy8Tc50msB8MwXBG3vW4-o",
+  authDomain: "fitting-room-9df39.firebaseapp.com",
+  projectId: "fitting-room-9df39",
+  storageBucket: "fitting-room-9df39.appspot.com",
+  messagingSenderId: "970421346792",
+  appId: "1:970421346792:web:834a499da903cbb7dcc45d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
