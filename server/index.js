6426988c69ef09acf3dd5061b4a575d4437f23fe
apaskara <apaskara@scs.ryerson.com>
2022-03-16 11:56:24 -0400

    branch1 commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { MongoClient } = require('mongodb');
+dotenv.config();
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+async function connect() {
+    MongoClient.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(console.log('connected db')).catch((err) => console.log(err))
+}
+
+var conn = await connect();
+
+app.post("/api/signup", async (req, res) => {
+    const email = req.body.email;
+    const businessName = req.body.businessName;
+    const password = req.body.password;
+    conn.collection('users').insertOne({ email: email, businessname: businessName, password: password }, (err, result) => {
+        console.log(result)
+    })
+})
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log("listening on port" + PORT)
+});
---
+++ server/schemas/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        businessName: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            max: 20,
+        }
+    }
+)
+
+module.exports = mongoose.model("User", UserSchema);
