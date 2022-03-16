90bf3628d4be382a6df6cdf897a85fbb938e86e2
Syd Shields <sshields750@gmail.com>
2022-03-16 11:01:55 -0400

    Added schema definitions

--- server/schemas/User.js
+++ server/schemas/User.js
@@ -1,1 +1,19 @@
 //this is the schema for business registration
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        businessName: {
+            type: String,
+            required: true,
+        }
+    }
+)
+
