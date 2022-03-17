c5b497e13047cc06d28b77089f8660e71b62eadb
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-03-17 14:39:18 -0400

    font weights etc

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,30 +4,31 @@ const Info = () => {
     return (
         <section className="h-100 bg-gray-100 p-4 flex-column justify-center align-content-center">
             
-                
-            <h3 className="mb-3 text-secondary">So ... how does this all work?</h3>
+            <div className="mw-85">
+            <h3 className="mb-4 text-secondary">So ... how does this all work?</h3>
 
-                <div className="flex-row align-center justify-between mb-3">
-                    <span className="mr-3 text-secondary"><h1>01</h1></span>
+                <div className="flex-row align-center justify-between mb-4">
+                    <span className="mr-2 text-secondary big-text">01</span>
                     <p className="mw-85">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce libero risus, vehicula ut tellus quis, tincidunt luctus ligula. Nunc non lectus vel ante placerat bibendum.
                         In vehicula, mauris at hendrerit hendrerit, urna nunc lobortis risus, at suscipit leo velit eget odio.
                     </p>
                 </div>
             
-            <div className="flex-row align-center justify-between mb-3">
+            <div className="flex-row align-center justify-between mb-4">
                 <p className="mw-85 text-right">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce libero risus, vehicula ut tellus quis, tincidunt luctus ligula. Nunc non lectus vel ante placerat bibendum.
                     In vehicula, mauris at hendrerit hendrerit, urna nunc lobortis risus, at suscipit leo velit eget odio.
                 </p>
-                <span className="ml-3 text-secondary"><h1>02</h1></span>
+                    <span className="ml-2 text-secondary big-text">02</span>
             </div>
             <div className="flex-row align-center justify-between">
-                <span className="mr-3 text-secondary"><h1>03</h1></span>
+                    <span className="mr-2 text-secondary big-text">03</span>
                 <p className="mw-85">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce libero risus, vehicula ut tellus quis, tincidunt luctus ligula. Nunc non lectus vel ante placerat bibendum.
                     In vehicula, mauris at hendrerit hendrerit, urna nunc lobortis risus, at suscipit leo velit eget odio.
                 </p>
+            </div>
             </div>
 
         </section>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -8,17 +8,17 @@ const SignUp = ({ isModalOpen, onModalChange }) => {
 
     return (
         <section className={`h-100 modal-screen align-center justify-center ${isModalOpen ? 'flex-row' : 'd-none'}`}>
-            <form id="signUp" className="flex-column bg-white w-50 p-3">
+            <form id="signUp" className="flex-column bg-white w-50 p-4">
             <div className="flex-row justify-between align-center mb-2">
                     <h3>Sign up for Early Access!</h3><span className="close-btn ml-2" onClick={handleModalChange}>×</span>
             </div>
                 <label for="email" className="d-none">Email</label>
-                <input id="email" name="email" type="email" placeholder="Email" className="mt-2 p-1" required />
+                <input id="email" name="email" type="email" placeholder="Email" className="mt-2 p-2" required />
                 <label for="organization" className="d-none">Organization</label>
-                <input id="organization" name="organization" type="text" placeholder="Organization" className="mt-2 p-1" required />
+                <input id="organization" name="organization" type="text" placeholder="Organization" className="mt-2 p-2" required />
                 <label for="phoneNo" className="d-none">Phone Number</label>
-                <input id="phoneNo" name="phoneNo" placeholder="Phone Number" className="mt-2 p-1" required />
-                <button type="submit" id="signUpBtn" className="mt-2" value="Create Account">
+                <input id="phoneNo" name="phoneNo" placeholder="Phone Number" className="mt-2 p-2" required />
+                <button type="submit" id="signUpBtn" className="mt-2">
                     Yes, I'd like to receive updates!
                 </button>
 
