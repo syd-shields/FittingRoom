b83efd61c174056a58a97b3931e97e3185abca80
apaskara <apaskara@scs.ryerson.com>
2022-03-18 09:46:07 -0400

    connected back end to db, learned about env file

--- server/index.js
+++ server/index.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { MongoClient } = require('mongodb');
+
+const signupRoute = require('./signup.js')
+
 dotenv.config();
 app.use(cors());
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
-async function connect() {
-    MongoClient.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(console.log('connected db')).catch((err) => console.log(err))
-}
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log('connected db')).catch((err) => console.log(err))
 
-var conn = await connect();
+app.use('/api/v1', signupRoute)
 
-app.post("/api/newsletter", async (req, res) => {
-    const email = req.body.email;
-    const businessName = req.body.businessName;
-    const phoneNumber = req.body.phoneNumber;
-    conn.collection('users').insertOne({ email: email, businessname: businessName, phoneNumber: phoneNumber }, (err, result) => {
-        console.log(result)
-    })
-})
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-    console.log("listening on port" + PORT)
+    console.log("listening on port " + PORT)
 });
