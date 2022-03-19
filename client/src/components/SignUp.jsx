09df95a5b135b394fdc835d9e20a3aa40bf5fbf1
apaskara <apaskara@scs.ryerson.com>
2022-03-19 14:02:14 -0400

    imported react-router-dom to navigate to different pages

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react'
 import axios from 'axios'
-
+import { useNavigate } from "react-router-dom";
 const SignUp = ({ isModalOpen, onModalChange }) => {
 
     const handleModalChange = useCallback(e => {
@@ -10,30 +10,29 @@ const SignUp = ({ isModalOpen, onModalChange }) => {
     const [email, setEmail] = useState('')
     const [businessName, setBusinessName] = useState('')
     const [phoneNumber, setPhoneNumber] = useState('')
-    
+
     const signupHandler = async () => {
-        try{
-        const res =  await axios.post('http://localhost:8000/api/v1/signup', {
-            email: email,
-            businessName: businessName,
-            phoneNumber: phoneNumber
-        })
-        console.log(res.data);
+        try {
+            const res = await axios.post('http://localhost:8000/api/v1/signup', {
+                email: email,
+                businessName: businessName,
+                phoneNumber: phoneNumber
+            })
+            console.log(res.data);
         } catch (err) {
-        console.log(err)
+            console.log(err)
+        }
     }
-       }
 
     return (
         <section className={`h-100 modal-screen align-center justify-center ${isModalOpen ? 'flex-row' : 'd-none'}`}>
-            <form id="signUp" className="flex-column bg-white w-50 p-4"  onSubmit={(e) => 
-                {
+            <form id="signUp" className="flex-column bg-white w-50 p-4" onSubmit={(e) => {
                 e.preventDefault();
                 signupHandler()
-              }}>
-            <div className="flex-row justify-between align-center mb-2">
+            }}>
+                <div className="flex-row justify-between align-center mb-2">
                     <h3 className="text-gray-400">Sign up for Early Access!</h3><span className="close-btn ml-2" onClick={handleModalChange}>×</span>
-            </div>
+                </div>
                 <label for="email" className="d-none">Email</label>
                 <input id="email" name="email" type="email" placeholder="Email" className="mt-2 p-2" onChange={(e) => setEmail(e.target.value)} required />
                 <label for="organization" className="d-none">Organization</label>
--- server/signup.js
+++ server/signup.js
@@ -3,7 +3,7 @@ const User = require('./schemas/User.js');
 const bcrypt = require("bcrypt");
 
 router.post("/signup", async (req, res) => {
-    try{
+    try {
         const newUser = new User({
             email: req.body.email,
             businessName: req.body.businessName,
@@ -12,9 +12,9 @@ router.post("/signup", async (req, res) => {
         const user = await newUser.save();
         res.status(200).json(user);
         console.log("user created")
-    } catch(err){
+    } catch (err) {
         res.status(500).json(err)
-    }   
+    }
 })
 
 module.exports = router;
