c555c673e8031a4d920b802b349e7ffcc7f1dea7
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-03-23 13:26:59 -0400

    fix bolding;

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -12,7 +12,7 @@ const Heading = ({ isModalOpen, onModalChange }) => {
             {/* <img className='hero-img cover' src={require('../assets/imgs/fashion-1.jpeg')} alt='Clothing Rack' /> */}
 
             <nav className="flex-row justify-between align-center">
-                <h3><b>style</b>me</h3>
+                <h3>style<b>me</b></h3>
                 <h4>
                     <a href="mailto:support@styleme.com">
                         support@styleme.com
@@ -20,7 +20,7 @@ const Heading = ({ isModalOpen, onModalChange }) => {
                 </h4>
             </nav>
             <div>
-                <h1><span className="bolder">style</span><span className="unbold">me</span></h1>
+                <h1><span className="unbold">style</span><span className="bolder">me</span></h1>
                 <h2>Now you can get personal style consultations from home!</h2>
             </div>
             <h4>
