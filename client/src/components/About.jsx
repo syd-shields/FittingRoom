61660370d28409ceeb3c5b33f7c9e59ecfc85035
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-03-24 16:06:28 -0400

    edit about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
                 <h3 className='text-center-sm'>Who are we?</h3>
                 <div className="flex-row justify-between justify-center-sm mt-3">
                 <p className="mw-60 mw-100-sm text-center-sm">
-                    We're here to bring the personal connection back to online retail.
+                    We're here to bring the personal connection back to your shopping experience.
                     <br /> <br />
-                    We are a team of expert stylists and fashion curators ready to bring personal consultation to the digital age. Our stylists have worked with a diverse group of clients, of all body types and personal styles. We'll match our stylists to your unique needs so that you can start get personalized, real-time fashion advice while shopping online.
+                    We are an agency of expert stylists and fashion curators ready to bring personal consultation to the digital age. Our team has worked with a diverse group of clients, of all body types and personal styles. With the help of our service and 24/7 support staff your style troubles will be a thing of the past.
                 </p>
                 <div>
                     <img src={require('../assets/imgs/fashion-2.jpeg')} className='about-img cover d-none-sm' alt="Stylist showing clothing on video call"/>
